c4ed640448919e09e7415baa75e763ec29fc014e
Gerald Mücke <gerald@inkstand.io>
2015-05-20 16:39:33 +0200

    added xxe example

--- examples/xxe-attack-example/src/main/java/whoopdicity/example/xxe/DocumentProcessor.java
+++ examples/xxe-attack-example/src/main/java/whoopdicity/example/xxe/DocumentProcessor.java
@@ -1,8 +1,64 @@
 package whoopdicity.example.xxe;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.IOException;
+import java.net.URL;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 /**
  * Created by gerald on 20.05.15.
  */
 public class DocumentProcessor {
 
+    /**
+     * Parses the document at the specified location and retrieves the value using the given xpath expression. The
+     * implementation is vulnerable to the XXE attack
+     * @param url
+     * @param xpathExpression
+     * @return
+     */
+    public static String evaluateVulnerable(final URL url, final String xpathExpression)
+            throws ParserConfigurationException, XPathExpressionException, IOException, SAXException {
+
+        //parse the document
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        final Document document = parse(url, factory);
+
+        return evaluate(xpathExpression, document);
+    }
+
+    /**
+     * Parses the document at the specified location and retrieves the value using the given xpath expression. The
+     * document does not allow doctype declarations
+     * @param url
+     * @param xpathExpression
+     * @return
+     */
+    public static String evaluateNoDTD(final URL url, final String xpathExpression)
+            throws ParserConfigurationException, XPathExpressionException, IOException, SAXException {
+
+        //parse the document
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        final Document document = parse(url, factory);
+
+        return evaluate(xpathExpression, document);
+    }
+
+    private static Document parse(final URL url, final DocumentBuilderFactory factory)
+            throws SAXException, IOException, ParserConfigurationException {
+
+        return factory.newDocumentBuilder().parse(url.openStream());
+    }
+
+    private static String evaluate(final String xpathExpression, final Document document)
+            throws XPathExpressionException {
+
+        return XPathFactory.newInstance().newXPath().evaluate(xpathExpression, document);
+    }
 }
--- examples/xxe-attack-example/src/test/java/whoopdicity/example/xxe/DocumentProcessorTest.java
+++ examples/xxe-attack-example/src/test/java/whoopdicity/example/xxe/DocumentProcessorTest.java
@@ -1,5 +1,7 @@
 package whoopdicity.example.xxe;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,11 +9,12 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import sun.misc.IOUtils;
+import org.xml.sax.SAXParseException;
 
 /**
  * Created by gerald on 20.05.15.
@@ -28,38 +31,85 @@ public class DocumentProcessorTest {
     }
 
     @Test
-    public void testProcessDocumentVulnerable_secretRevealed() throws Exception {
+    public void testProcessDocument_vulnerable_secretRevealed() throws Exception {
         //prepare
         //the attacked file containing the secret
+        final File attackedFile = prepareAttackedFile();
+        final File maliciousFile = prepareMaliciousFile(attackedFile);
+
+        String secretContent = DocumentProcessor.evaluateVulnerable(maliciousFile.toURI().toURL(),
+                                                                    "//document/property");
+
+        //assert
+        //the content from the attacked file is put into the attacking xml where it resolves to the hostname
+        assertEquals("secretContent", secretContent);
+    }
+
+    @Test(expected = SAXParseException.class)
+    public void testProcessDocument_dtdDisabled_exception() throws Exception {
+        //prepare
+        //the attacked file containing the secret
+        final File attackedFile = prepareAttackedFile();
+        final File maliciousFile = prepareMaliciousFile(attackedFile);
+
+        String secretContent = DocumentProcessor.evaluateNoDTD(maliciousFile.toURI().toURL(), "//document/property");
+
+        //assert
+        //the content from the attacked file is put into the attacking xml where it resolves to the hostname
+        assertEquals("secretContent", secretContent);
+    }
+
+    /**
+     * Creates a temporary file containing a secret
+     * @return
+     *  the handle of the file.
+     * @throws IOException
+     */
+    private File prepareAttackedFile() throws IOException {
+
         final File attackedFile = folder.newFile("attackedFile.txt");
         try (FileOutputStream fos = new FileOutputStream(attackedFile)) {
             IOUtils.write("secretContent", fos);
         }
+        return attackedFile;
+    }
+
+    /**
+     * Creates a temporary file containing an external entity definition pointing to the attacked filed.
+     * In this test scenario, the file is a temporary file. In a real-world attack, it would be the location
+     * of a well known file containing a secret, like /etc/passwd
+     * @param attackedFile
+     *  the target file whose content should be read
+     * @return
+     *  the handle of the malicious file
+     * @throws IOException
+     */
+    private File prepareMaliciousFile(final File attackedFile) throws IOException {
+
         //as attacker file we use a template and replacing a %s placeholder with the url of the attacked file
         //in a real-world attack we would use a valuable target such as /etc/passwd
         final File attackerFile = folder.newFile("attackerFile.xml");
 
         //load the template file from the classpath
-        try (InputStream is = getClass().getResourceAsStream("RemoteContentRepositoryTest_attacker.xml");
+
+        try (InputStream is = DocumentProcessorTest.class.getResourceAsStream("/malicious_template.xml");
              FileOutputStream fos = new FileOutputStream(attackerFile)) {
 
-            final String attackerContent = prepareAttackerContent(is, attackedFile);
+            final String attackerContent = prepareMaliciousContent(is, attackedFile);
             IOUtils.write(attackerContent, fos);
         }
-
-        System.setProperty("arquillian.launch", "testContainer");
-        subject.onArquillianHost(attackerFile.toURI().toURL());
-        subject.setupManually();
-
-        //act
-        subject.before();
-
-        //assert
-        //the content from the attacked file is put into the attacking xml where it resolves to the hostname
-        assertEquals("secretContent", subject.getRemoteHost());
+        return attackerFile;
     }
 
-    private String prepareAttackerContent(final InputStream templateInputStream, final File attackedFile)
+    /**
+     * Read the content from the input stream and replace the placeholder in the template with the
+     * URL of the attacked file
+     * @param templateInputStream
+     * @param attackedFile
+     * @return
+     * @throws IOException
+     */
+    private String prepareMaliciousContent(final InputStream templateInputStream, final File attackedFile)
             throws IOException {
 
         final StringWriter writer = new StringWriter();
